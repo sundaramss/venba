aac92f06488512b6327abfe51145c0b29ab37080
sundaramss <1131710+sundaramss@users.noreply.github.com>
2018-09-25 21:16:21 +0530

    CSS Changes

--- yappu-illakkanam/src/cljs/yappu_ilakkanam/appcss.cljs
+++ yappu-illakkanam/src/cljs/yappu_ilakkanam/appcss.cljs
@@ -1,32 +1,29 @@
 (ns yappu-ilakkanam.appcss
   (:require [cljss.reagent :as cr :refer-macros [defstyled]]
-            [cljss.core :refer [inject-global]]))
+            [cljss.core :refer [inject-global defstyles]]))
 (defn global-css []
  (inject-global
   {:container {
-                     :width "100%"
-                     :max-width "1800px"
-                     :margin  " 0 auto"
-                     :font-family "Helvetica"
-                     :font-size "14px"}}))
-(defstyled LOGO :img
- {:width "200px"
-  :height "auto"})
+                :width "100%"
+                :max-width "1400px"
+                :margin  " 0 auto"
+                :font-family "Helvetica"
+                :font-size "14px"}}))
 
-(defstyled H-MENUS :div
+(defstyles H-MENUS []
  {:min-height "35px"
   :width "100%"
   :background "#ad6e47"})
 
-(defstyled H-M-ROW :ul
+(defstyles H-M-ROW []
  { :padding-left "16px"
    :text-align "right"});
 
-(defstyled H-MENU :li
+(defstyles H-MENU []
  { :display "inline-block"
    :padding "8px 4px 0"})
 
-(defstyled H-M-LINK :a
+(defstyles H-M-LINK []
    {:color "#fff"
     :text-decoration "none"
     :font-size "12px"})
@@ -35,3 +32,7 @@
   {:font-size "38px" ;; reads the value and removes custom `:size` attribute
    :color (with-meta #(get {:light "#fff" :dark "#000"} %) :color) ;; gets `:color` value and removes this attribute
    :padding (with-meta #(str %1 " " %2) [:padding-v :padding-h])}) ;; gets values of specified attrs as arguments and remove those attrs
+
+(defstyles CONTENT []
+  { :margin "0 auto"
+    :background "#efefef"});
